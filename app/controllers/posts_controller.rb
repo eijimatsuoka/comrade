a78cce5a85e7cd7725c73741a1eeb0edd5ee33c6
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-12-05 19:35:22 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :image, :text)
+    params.require(:post).permit(:title, :image, :text).merge(user_id: current_user.id)
   end
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -34,11 +34,12 @@ ActiveRecord::Schema.define(version: 2020_12_04_181038) do
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
-    t.string "image"
+    t.string "title", null: false
+    t.string "text", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -55,4 +56,5 @@ ActiveRecord::Schema.define(version: 2020_12_04_181038) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
