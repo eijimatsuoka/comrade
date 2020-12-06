9dcbbe0cd9b35e60dd90c1bcd101d939eb4bf910
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-12-06 21:54:17 +0900

    コメント機能の導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -13,6 +13,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
   end
 
   def edit
