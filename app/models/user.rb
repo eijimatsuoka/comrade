98263c8ba9f445be1c5d8feac4c6e65443055a06
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-12-07 07:03:25 +0900

    ルーム管理機能の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
   has_many :comments
+  has_many :room_users
+  has_many :rooms, through: :room_users
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
