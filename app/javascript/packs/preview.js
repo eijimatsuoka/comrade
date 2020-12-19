23f4aa352a83c26c59a9a792b59c98500ba89a68
eijimatsuoka <t.jgreeeen550229@icloud.com>
2020-12-19 19:17:38 +0900

    微修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -9,7 +9,7 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     blobImage.setAttribute('src', blob);
     imageElement.appendChild(blobImage);
     ImageList.appendChild(imageElement);
-       };
+    };
     document.getElementById('post-image').addEventListener('change', function(e){
       const imageContent = document.querySelector('img');
       if (imageContent){
